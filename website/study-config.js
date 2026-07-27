9e5e2c43b07e1ab310fb5534202a8013a652a4f3
Lynxyao <Lynxyao@foxmail.com>
2026-07-28 02:03:54 +0800

    Enable anonymous pilot tracking

--- website/study-config.js
+++ website/study-config.js
@@ -1,6 +1,6 @@
 window.STUDY_CONFIG = {
-  enabled: false,
-  supabaseUrl: "",
-  supabaseAnonKey: "",
+  enabled: true,
+  supabaseUrl: "https://oxnudjtgzboipvzdmnmh.supabase.co",
+  supabaseAnonKey: "sb_publishable_bU_3th2scbfTPX_pl-PYDw_mjXjD_Ho",
   consentVersion: "pilot-2026-07",
 };
